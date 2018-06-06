48cc43fee33b09655699dec002e111969c22760b
Erin Brown <emberbrown@gmail.com>
2018-06-05 22:54:53 -0400

    Adds response example

---
+++ src/tests/responseExample.js
@@ -0,0 +1,20 @@
+export default {
+  idMeal: '1',
+  strArea: 'Italian',
+  strCategory: 'Pasta',
+  strIngredient1: 'Spaghetti',
+  strIngredient2: 'Olive Oil',
+  strIngredient3: 'Onion',
+  strIngredient4: '',
+  strInstructions:
+    'Cook the pasta, following pack instructions. Heat the oil in a non-stick frying pan and cook the onion, garlic and chilli for 3-4 mins to soften. Stir in the tomato purée and cook for 1 min, then add the pilchards with their sauce. Cook, breaking up the fish with a wooden spoon, then add the olives and continue to cook for a few more mins. Drain the pasta and add to the pan with 2-3 tbsp of the cooking water. Toss everything together well, then divide between plates and serve, scattered with Parmesan.',
+  strMeal: 'Pilchard puttanesca',
+  strMealThumb:
+    'https://www.themealdb.com/images/media/meals/vvtvtr1511180578.jpg',
+  strMeasure1: '300g',
+  strMeasure2: '1 tbls',
+  strMeasure3: '1 finely chopped ',
+  strMeasure4: '',
+  strSource: 'https://www.bbcgoodfood.com/recipes/pilchard-puttanesca',
+  strYoutube: 'https://www.youtube.com/watch?v=wqZzLAPmr9k'
+};
